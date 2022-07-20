7e3962daf7cafa3a16319f937580a4cba957be03
zjn-wsc-ywh-amx <2830851361@qq.com>
2022-07-20 22:31:13 +0800

    add forge

--- Forged_Satoshi_Signature/forged.py
+++ Forged_Satoshi_Signature/forged.py
@@ -22,7 +22,7 @@ def inverse(a, m):  # 这个扩展欧几里得算法求模逆
         v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
     return u1 % m
 
-def sign(e, k, d, P, p):
+def sign(e, k, d, P, p):    # e: H(message)  k randomly chosen 0 < k < q
     Q = k*P
     s = np.mod(inverse(k, p)*(e + d*Q.x), p)
     return s, Q.x
@@ -62,3 +62,26 @@ k = random.randrange(1,order-1)
 #r = signature.r
 #s = signature.s
 #print(r,s)
+
+
+F = GF (0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F)  #生成一个给定阶数的全局唯一有限域
+C = EllipticCurve ([F (0), F (7)])     #构造出椭圆曲线
+
+
+G = C.lift_x(0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798)
+N = GF (C.order())
+P = P=-C.lift_x(0x11db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5c) # block 9 coinbase payout key.
+
+def forge(c, a=-1):  # Create a forged 'ECDSA'  (hashless) signature
+  # set a to something other than -1 to be less obvious
+  a = N(a)
+  R = c*G + int(a)*P
+  s = N(int(R.xy()[0]))/a
+  m = N(c)*N(int(R.xy()[0]))/a
+  print ('hash1 = %d'%m)
+  print ('r1 = %d'%(int(R.xy()[0])))
+  print ('s1 = %d'%s)
+def verify():
+    pass
+for c in range(1,10):
+  forge(c)
